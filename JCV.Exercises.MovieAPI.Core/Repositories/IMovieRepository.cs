fd387f2a897920cafc6b89067ef8f651c4a80094
John Vivian <reiatwork@hotmail.co.uk>
2021-02-15 03:39:14 +0000

    Initial Project Layout

---
+++ JCV.Exercises.MovieAPI.Core/Repositories/IMovieRepository.cs
@@ -0,0 +1,13 @@
+﻿using JCV.Exercises.MovieAPI.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCV.Exercises.MovieAPI.Core.Repositories
+{
+    public interface IMovieRepository
+    {
+        Task Create(MovieInfo);
+    }
+}
